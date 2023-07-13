b95a5e81c5aa085b57f560c52889799c597281aa
abi4kit <abi4kit@gmail.com>
2023-07-12 22:06:53 -0700

    add stripe schema on user, stripe webhook started

---
+++ src/pages/api/stripe/webhook-handlers.ts
@@ -0,0 +1,69 @@
+import type { PrismaClient } from "@prisma/client";
+import type Stripe from "stripe";
+
+
+// retrieves a Stripe customer id for a given user if it exists or creates a new one
+export const getOrCreateStripeCustomerIdForUser = async ({
+    stripe,
+    prisma,
+    userId,
+}: {
+    stripe: Stripe;
+    prisma: PrismaClient;
+    userId: string;
+}) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    });
+  
+    if (!user) throw new Error("User not found");
+  
+    if (user.stripeCustomerId) {
+        return user.stripeCustomerId;
+    }
+
+     // create a new customer
+  const customer = await stripe.customers.create({
+    email: user.email ?? undefined,
+    name: user.name ?? undefined,
+    // use metadata to link this Stripe customer to internal user id
+    metadata: {
+      userId,
+    },
+  });
+
+  // update with new customer id
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      stripeCustomerId: customer.id,
+    },
+  });
+
+  if (updatedUser.stripeCustomerId) {
+    return updatedUser.stripeCustomerId;
+  }
+    
+
+}
+export const handlePaymentIntentSucceeded = async ({
+    event, stripe, prisma
+}:{event: Stripe.Event, stripe: Stripe, prisma: PrismaClient}) => {
+    const paymentIntent = event.data.object as Stripe.PaymentIntent;
+    const userId = paymentIntent.metadata.userId
+
+    //update user with payment intent data
+    await prisma.user.update({
+        where: {
+            id: userId,
+        },
+        data: {
+            stripePaymentIntentId: paymentIntent.id,
+            stripePaymentIntentStatus: paymentIntent.status,
+        }
+    })
+}
